f562447b54307540aad64d21d399ca82d7c0879c
Keller Wang <yuntianwang2003@gmail.com>
2025-02-23 03:41:41 +0800

    small fix

--- src/app/testingPage/page.js
+++ src/app/testingPage/page.js
@@ -20,22 +20,6 @@ export default function TestingPage() {
                 <div className="w-full h-[2px] bg-gray-700"></div>
             </header>
             {/* Testing Area */}
-            <div className="h-[65dvh] w-2/3 mx-auto text-center text-3xl font-oxanium grid place-items-center text-black">
-                <h1>
-                    <span>Create</span>
-                    <span className="text-blue-500"> a continuous feedback loop </span>
-                    <span>where user experiences shape accessibility scores and drive better design.</span>
-                </h1>
-            </div>
-            <ScrollingImages />
-            <div className="h-[65dvh] w-2/3 mx-auto text-center text-3xl font-oxanium grid place-items-center">
-                <h1>
-                    But... <br />
-                    What is this feedback loop and accessibility score? <br />
-                    How does it work? <br />
-                    And why do we need it?
-                </h1>
-            </div>
             <Footer />
         </ContentWrapper>
 
