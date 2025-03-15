5afd74d6715f400d5dff54b0ce7f5d2979ebd187
Keller Wang <yuntianwang2003@gmail.com>
2025-03-15 21:21:04 +0800

    bug fix

--- src/app/[locale]/testingPage/page.js
+++ src/app/[locale]/testingPage/page.js
@@ -1,13 +1,6 @@
 'use client'
-import AnimatedLogo from "@/components/homeLanding";
 import ContentWrapper from "@/components/contentWrapper";
-import ProjectCover from "@/components/projectCover";
-import Footer from "@/components/footer";
-import { Divide as Hamburger } from 'hamburger-react'
 import { useState } from "react";
-import TiltedCard from "@/blocks/Components/TiltedCard/TiltedCard";
-import Image from "next/image";
-import ScrollingImages from "@/components/projectPages/scrollingImages";
 
 export default function TestingPage() {
 
